f379c4a3049572988bc4ea7cce2e1a0bfce48959
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-01-29 21:02:43 -0300

    feat: adicionar endpoint POST para cadastro de autores

--- src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
@@ -1,10 +1,42 @@
 package io.github.cursodsousa.libraryapi.controller;
 
+import io.github.cursodsousa.libraryapi.dto.AutorDTO;
+import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.service.AutorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("autores")
 // http://localhost:8080/autores
 public class AutorController {
+
+    private final AutorService service;
+
+    public AutorController(AutorService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor){
+        Autor autorEntidade = autor.mapearParaAutor();
+        service.salvar(autorEntidade);
+
+
+        // http://localhost:8080/autores/d7846opyt
+        URI location =
+                ServletUriComponentsBuilder.
+                fromCurrentContextPath()
+                .path("/{id}")
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 }
