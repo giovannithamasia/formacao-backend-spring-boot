65cdcd430f9b366563a2965fb9813b50ba5efa8e
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-01-31 17:44:57 -0300

    chore: adiciona queries para filtrar autores por nome e nacionalidade

--- src/main/java/io/github/cursodsousa/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/cursodsousa/libraryapi/repository/AutorRepository.java
@@ -6,11 +6,16 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
 
-    Autor findByNome(String nome);
+    List<Autor> findByNome(String nome);
+
+    List<Autor> findByNacionalidade(String nacionalidade);
+
+    List<Autor> findByNomeAndNacionalidade(String nome,String nacionalidade);
 
     @Query(" select a from Autor a where a.id = ?1")
     Autor buscandoPorId (UUID idProcurado);
