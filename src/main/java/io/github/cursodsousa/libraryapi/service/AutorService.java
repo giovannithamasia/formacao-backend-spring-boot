7bffb7c73352d4a474ddea6546d3b8f899c37ac7
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-02-07 20:24:05 -0300

    feat: implementar busca de autores por nome e nacionalidade usando Query By Example

--- src/main/java/io/github/cursodsousa/libraryapi/service/AutorService.java
+++ src/main/java/io/github/cursodsousa/libraryapi/service/AutorService.java
@@ -6,6 +6,8 @@ import io.github.cursodsousa.libraryapi.repository.AutorRepository;
 import io.github.cursodsousa.libraryapi.repository.LivroRepository;
 import io.github.cursodsousa.libraryapi.validator.AutorValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -62,6 +64,22 @@ public class AutorService {
 
         return repository.findAll();
     }
+
+    public List<Autor> pesquisaByExample(String nome,String nacionalidade){
+        var autor = new Autor();
+        autor.setNome(nome);
+        autor.setNacionalidade(nacionalidade);
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example<Autor> autorExample = Example.of(autor,matcher);
+        return repository.findAll(autorExample);
+    }
+
 
     public boolean possuirLivro(Autor autor){
         return livroRepository.existsByAutor(autor);
