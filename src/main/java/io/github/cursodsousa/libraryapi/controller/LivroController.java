5758b1fed2086b856fbb6299c1a86e3a759c8e5d
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-02-19 21:29:57 -0300

    feat: adiciona endpoint para cadastro de livro

--- src/main/java/io/github/cursodsousa/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/LivroController.java
@@ -1,8 +1,8 @@
 package io.github.cursodsousa.libraryapi.controller;
 
+import io.github.cursodsousa.libraryapi.controller.mappers.LivroMapper;
 import io.github.cursodsousa.libraryapi.dto.CadastroLivroDTO;
-import io.github.cursodsousa.libraryapi.dto.ErroResposta;
-import io.github.cursodsousa.libraryapi.exceptions.RegistroDuplicadoException;
+import io.github.cursodsousa.libraryapi.model.Livro;
 import io.github.cursodsousa.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,21 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("livros")
 @RequiredArgsConstructor
-public class LivroController {
+public class LivroController implements GenericController {
 
     private final LivroService service;
+    private final LivroMapper mapper;
 
     @PostMapping
-    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto){
-        try{
-            //mapear dto para entidade
-            //enviar a entidade para o service validar e salvar na base
-            //criar url para acesso dos dados do livro
-            //retornar codigo created com header location
-            return ResponseEntity.ok(dto);
-        }catch(RegistroDuplicadoException e){
-            var erroDto = ErroResposta.conflito(e.getMessage());
-            return ResponseEntity.status(erroDto.status()).body(erroDto);
-        }
+    public ResponseEntity<Void> salvar(@RequestBody @Valid CadastroLivroDTO dto){
+            Livro livro = mapper.toEntity(dto);
+            service.salvar(livro);
+            var url = gerarHeaderLocation(livro.getId());
+            return ResponseEntity.created(url).build();
     }
 }
