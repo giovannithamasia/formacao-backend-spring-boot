f57a23d91d68e0d1896d2af37d297067edd4de81
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-02-21 17:40:23 -0300

    feat: adiciona endpoint para obter detalhes do livro

--- src/main/java/io/github/cursodsousa/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/LivroController.java
@@ -2,15 +2,15 @@ package io.github.cursodsousa.libraryapi.controller;
 
 import io.github.cursodsousa.libraryapi.controller.mappers.LivroMapper;
 import io.github.cursodsousa.libraryapi.dto.CadastroLivroDTO;
+import io.github.cursodsousa.libraryapi.dto.ResultadoPesquisaLivroDTO;
 import io.github.cursodsousa.libraryapi.model.Livro;
 import io.github.cursodsousa.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("livros")
@@ -26,5 +26,15 @@ public class LivroController implements GenericController {
             service.salvar(livro);
             var url = gerarHeaderLocation(livro.getId());
             return ResponseEntity.created(url).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResultadoPesquisaLivroDTO> obterDetalhes(
+            @PathVariable("id") String id){
+        return service.obterPorId(UUID.fromString(id))
+                .map(livro -> {
+                    var dto = mapper.toDto(livro);
+                    return ResponseEntity.ok(dto);
+                }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
