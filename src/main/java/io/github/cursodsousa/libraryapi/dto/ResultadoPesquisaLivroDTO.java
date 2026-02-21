1e2bbf9fa022c1915a653ae8e6343b46df27c003
giovannithamasia <giovanni_thamasia@estudante.sesisenai.org.br>
2026-02-21 17:36:53 -0300

    fix: ajusta campos do Dto de resultado da pesquisa de livros

--- src/main/java/io/github/cursodsousa/libraryapi/dto/ResultadoPesquisaLivroDTO.java
+++ src/main/java/io/github/cursodsousa/libraryapi/dto/ResultadoPesquisaLivroDTO.java
@@ -7,12 +7,12 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record ResultadoPesquisaLivroDTO(
+        UUID id,
         String isbn,
         String titulo,
         LocalDate dataPublicacao,
         GeneroLivro genero,
         BigDecimal preco,
-        UUID idAutor,
         AutorDTO autor
 ) {
 }
